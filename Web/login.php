bfdf843e757e7f5da64894371c5269b75e5c0632
unknown <w.chulaket@gmail.com>
2017-10-21 00:42:44 +0700

    connected database success!!!

--- Web/login.php
+++ Web/login.php
@@ -1,13 +1,30 @@
-<?php 
-if(isset($_POST['submit'])) {
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
-	if($user == "admin" && $pass == "admin") {
-		header("location:adminPage.html");
+<?php
+	session_start();
+	mysql_connect("localhost","cs57160040","Dt8R7ASn2pAEuLVL");
+	mysql_select_db("cs57160040");
+	$strSQL = "SELECT * FROM member WHERE Username = '".mysql_real_escape_string($_POST['user'])."' 
+	and Password = '".mysql_real_escape_string($_POST['pass'])."'";
+	$objQuery = mysql_query($strSQL);
+	$objResult = mysql_fetch_array($objQuery);
+	if(!$objResult)
+	{
+			echo "Username and Password Incorrect!";
 	}
-	else {
-		echo "Failed!!!";
-	}
-}
+	else
+	{
+			// $_SESSION["UserID"] = $objResult["UserID"];
+			$_SESSION["Status"] = $objResult["Status"];
 
- ?>
+			session_write_close();
+			
+			if($objResult["Status"] == "ADMIN")
+			{
+				header("location:admin_page.php");
+			}
+			else
+			{
+				header("location:user_page.php");
+			}
+	}
+	mysql_close();
+?>
