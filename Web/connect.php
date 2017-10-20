1ffc81bee3b2163415ba289ee5cfde526aa14e70
unknown <w.chulaket@gmail.com>
2017-10-20 23:47:53 +0700

    Last

--- Web/connect.php
+++ Web/connect.php
@@ -1,8 +1,22 @@
-<?php 	
+<?php 
+if (isset($_POST['submit'])) {
+
 	$hostname ="localhost";
 	$username ="cs57160040";
 	$password ="Dt8R7ASn2pAEuLVL";
 	$db_name="cs57160040";
-$link = mysql_connect($hostname,$username,$password) or die ('ติดต่อฐานข้อมูลไม่ได้');
-$db_selected = mysql_select_db($db_name, $link) or die ('ไม่พบฐานข้อมูล');
+ 	$conn = mysql_connect($hostname,$username,$password) or die ('ติดต่อฐานข้อมูลไม่ได้');
+ 	$db_selected = mysql_select_db($db_name, $conn) or die ('ไม่พบฐานข้อมูล');
+	
+
+	$user = $_POST['user'];
+	$pass = $_POST['pass'];
+
+	$sql = "SELECT * FROM member WHERE Username='$user' AND Password='$pass'";
+	$result = $conn->query($sql) or die ("Username or Password incorrect");
+	
+
+
+	$conn->close();
+}	
 ?>
